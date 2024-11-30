650341ae1ba4f3b4a567ab877359a07512820cae
jarrok3 <jarrok333@gmail.com>
2024-11-30 14:55:30 +0100

    minor code fixes, chunk size increase - resolution increase, error handling

--- audio_processing.py
+++ audio_processing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import time
 
 # Deklaracja zmiennych globalnych
-CHUNK = 1024 # Ilość próbek przetwarzana przy każdej operacji "read", 
+CHUNK = 1024 * 4 # Ilość próbek przetwarzana przy każdej operacji "read", 
 '''
 The CHUNK size determines how much data is read per stream.read() call. Larger chunks reduce CPU load but increase latency, while smaller chunks allow more responsive and real-time data handling but can be more demanding on CPU.
 For audio analysis or effects (like a Wah-Wah pedal), a smaller CHUNK (e.g., 512 or 1024) might be ideal for responsiveness, whereas for simple recording, a larger CHUNK (e.g., 4096) might improve stability.
@@ -37,8 +37,9 @@ def amplitude_to_db(amplitude):
     Returns:
         amplitude (decibels): wartość amplitudy wyznaczona w decybelach po konwersji
     """
-    # zabezpieczenie przed zwróceniem wartości 0, nieakceptowalnej do użycia w logarytmie
-    amplitude = np.maximum(amplitude / 32767.0, 1e-9)
+    # zabezpieczeni
+    # e przed zwróceniem wartości 0, nieakceptowalnej do użycia w logarytmie
+    amplitude = np.maximum(amplitude / 32767.0, 1e-9) # normalizacja wartości amplitudy z formatu 16bit (-32768,32767) do zakresu (-1,1)
     return 20 * np.log10(amplitude)
                          
                          
@@ -55,11 +56,19 @@ def update_plot(frame):
     global last_maxdb_update
     
     # Sczytanie danych z pojedynczej klatki
-    data = stream.read(CHUNK, exception_on_overflow=False)
-    audio_data = np.frombuffer(data, dtype=np.int16)
-
+    try:
+        data = stream.read(CHUNK, exception_on_overflow=False)
+    # Zabezpieczenie przed brakiem/niepoprawnym czytaniem danych ze źródła audio (wyzerowanie wykresów)
+    except IOError as err:
+        print(f'Wystąpił problem ze sczytaniem sygnału audio: {err}')
+        waveform_line.set_ydata(np.zeros)
+        spectrum_line.set_ydata(np.zeros)
+        return waveform_line,spectrum_line
+    
+    audio_data = np.frombuffer(data, dtype=np.int16) # konwersja danych do tablicy 16-bitowych integerów
+    
     # Przeprowadzenie szybkiej transformaty Fourier'a
-    fft_data = np.fft.fft(audio_data)
+    fft_data = np.fft.fft(audio_data) # przejście z domeny czasu na domenę częstotliwości (liczby złożone)
     frequencies = np.fft.fftfreq(len(fft_data), 1 / RATE)
 
     # Konwersja danych do decybeli
@@ -83,22 +92,28 @@ def update_plot(frame):
 if __name__ == "__main__": 
     p = pau.PyAudio() # deklaracja oraz inicjalizacja obiektu z biblioteki PyAudio klasy PyAudio
     
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK
-    ) # otwarcie kanalu glosowego
+    try:
+        stream = p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            input=True,
+            frames_per_buffer=CHUNK
+        ) # otwarcie kanalu glosowego
+    except OSError as err:
+        print(f'Podłącz urządzenie audio. Wystąpił błąd: {err}')
+        # zakończenie działania programu
+        quit()
+        
     
     print("Nagrywanie...") # informacja do terminala
     
     fig,(ax1,ax2) = plt.subplots(2,1,figsize=(18,12)) # tworzy rysunek "fig", zawierający dwa wykresy
     fig.canvas.mpl_connect("key_press_event", on_key) # połączenie "fig" z funkcją on_key
 
-    # # Konfiguracja wykresu przebiegu sygnała
+    # # Konfiguracja wykresu przebiegu sygnału
     # Zainicjowanie osi
-    x_waveform = np.arange(0, CHUNK) # ustalenie dostępnych wartości na osi odciętych
+    x_waveform = np.arange(0, 2*CHUNK,2) # ustalenie dostępnych wartości na osi odciętych
     y_waveform = np.zeros(CHUNK)
     
     # Zainicjowanie linii odwzorowującej przebieg sygnału
@@ -109,15 +124,15 @@ if __name__ == "__main__":
     ax1.set_xlabel('Próbki [-]')
     ax1.set_ylabel('Amplituda [-]')
     ax1.set_xlim(0, CHUNK)
-    ax1.set_ylim(-32768, 32767) # ograniczenie na osi rzędncych ze względu na format paInt16 sygnału wejściowego
+    ax1.set_ylim(-4096, 4096) # ograniczenie na osi rzędnych 
     
     # Dalsza konfiguracja wyglądu wykresu
-    for decade in range(-32000, 32000, 4000):
+    for decade in range(-4096, 4096, 128):
         ax1.axhline(y=decade, color='gray', linestyle='-', linewidth=0.5)
-    for semidecade in range(-32000, 32000, 1000):
+    for semidecade in range(-4096, 4096, 512):
         ax1.axhline(y=semidecade, color='lightgray', linestyle='-', linewidth=0.33)
-    for xdecade in range(0,2*CHUNK,120):
-        ax1.axvline(x=xdecade,color='lightgray',linestyle='-', linewidth=0.33)
+    for xdecade in range(0,4*CHUNK,128):
+        ax1.axvline(x=xdecade,color='gray',linestyle='-', linewidth=0.33)
     
     # # Konfiguracja wykresu częstotliwości f w zależności od poziomu natężenia L
     # Zainicjowanie osi
@@ -125,13 +140,13 @@ if __name__ == "__main__":
     y_spectrum = np.zeros(CHUNK // 2)
     
     # Zainicjowanie linii odwzorowującej spektrum częstotliwości sygnału
-    spectrum_line, = ax2.plot(x_spectrum, y_spectrum)
+    spectrum_line, = ax2.semilogx(x_spectrum, y_spectrum)
     
     # Edycja wykresu
     ax2.set_title('Analiza spektralna przebiegu sygnału')
     ax2.set_xlabel('Częstotliwość f(Hz)')
     ax2.set_ylabel('Poziom natężenia dźwięku L(dB)')
-    ax2.set_xlim(0, RATE // 2)
+    ax2.set_xlim(20, RATE / 2)
     ax2.set_ylim(-50, 50)  # Badany zakres częstotliwości
     
     # Dalsza konfiguracja wyglądu wykresu
@@ -143,9 +158,8 @@ if __name__ == "__main__":
         ax2.axvline(x=xdecade,color='lightgray',linestyle='-', linewidth=0.33)
         
     try:
-        while plt.fignum_exists(fig.number):
-            ani = animation.FuncAnimation(fig, update_plot, blit=True, interval=50)
-            plt.show()
+        ani = animation.FuncAnimation(fig, update_plot, blit=True, interval=50)
+        plt.show()
     except KeyboardInterrupt:
         stream.close() # zamknięcie kanału głosowego w wypadku naciśnięcia klawisza klawiatury przez użytkownika
         print("Zakonczono analize sygnalu")
@@ -157,8 +171,5 @@ Known problems:
     OSError: [Errno -9981] Input overflowed
 For Chunk size 512, the system cannot handle the sampling quickly enough, and the buffer gets overflown
 Had to change chunk size to 1024 to tackle the problem
-
-    struct.error
-For incorrect format handling (paint16 equals 2 bytes long sample)
 '''
 
